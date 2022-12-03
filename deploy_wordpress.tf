446ec6ea09604a44aff59ae17e38a28bba95d018
Aniket Dandekar <85037938+AniketDandekar@users.noreply.github.com>
2022-12-03 13:13:55 +0530

    Create deploy_wordpress.tf

---
+++ deploy_wordpress.tf
@@ -0,0 +1,57 @@
+# Terraform file for launching wordpress container on kubernetes
+
+
+# provider block
+provider "kubernetes" {
+    config_context = "minikube"
+}
+
+
+# resource block
+resource "kubernetes_deployment" "wps" {
+   metadata {
+       name = "wordpress"
+   }
+   spec {
+      replicas = 3
+      selector {
+           match_labels = {
+              "env" = "development" 
+              "loc" = "IN"
+              "site" = "Wordpress"
+           }
+      } 
+   template { 
+      metadata {
+         labels = {
+           "env" = "development"
+           "loc" = "IN"
+           "site" = "Wordpress"
+        }
+     }
+     spec {
+        container {
+           image = "wordpress"
+           name  = "wp"
+       }
+     }
+   }
+ }
+}
+
+resource "kubernetes_service" "wordpress_svc" {
+metadata {
+    name = "expose_wordpress"
+}
+spec {
+    selector = {
+        site =        kubernetes_deployment.wps.spec[0].template[0].metadata[0].labels.site
+   }
+   port {
+   node_port   = 30001
+   port        = 8080
+   target_port = 80
+  }
+  type = "NodePort"
+  }
+}
